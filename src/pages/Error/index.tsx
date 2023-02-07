3cdad26e626c1ce7264fee52c81c6703b6d6b371
Angelo Munhoz <angelomca09@gmail.com>
2023-02-06 22:57:54 -0300

    Adding an Error Page

---
+++ src/pages/Error/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.css"
+
+export default function ErrorPage() {
+  return (
+    <main id="error-page" className="container">
+      <hgroup>
+        <h1>404</h1>
+        <h2>Página não encontrada</h2>
+      </hgroup>
+    </main>
+  );
+}
