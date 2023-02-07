c07851f65b33bb53ec3a3e426ec7b68d5b45e9ef
Angelo Munhoz <angelomca09@gmail.com>
2023-02-07 00:56:49 -0300

    Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,50 @@
+
+import { useState, useEffect } from "react";
+import api from "../../api";
+import { IAlbum } from "../../interfaces/IAlbum";
+import { ISticker } from "../../interfaces/ISticker";
 import { Navbar } from "../../layout/Navbar"
+import { getUser } from "../../services/user"
+import { FaArrowRight } from "react-icons/fa"
 
 export const Dashboard = () => {
+
+  const user = getUser()!;
+
+  const [albums, setAlbums] = useState<IAlbum[]>([]);
+  const [stickers, setStickers] = useState<ISticker[]>([]);
+
+  useEffect(() => {
+    api.getUserById(user.profile?.id!).then(res => {
+      setAlbums(res.albums)
+      setStickers(res.stickers)
+    })
+  }, [])
+
   return (
     <>
       <Navbar />
       <main className="container">
         <div className="grid">
-          <article><h1>Álbuns</h1></article>
-          <article><h1>Figurinhas</h1></article>
+
+          <article className="contrast">
+            <div className="grid">
+              <h1>Meus Álbuns: {albums.length}</h1>
+              <div>
+                <button className="add outline contained rounded"><FaArrowRight /></button>
+              </div>
+            </div>
+          </article>
+
+          <article className="contrast">
+            <div className="grid">
+              <h1>Minhas Figurinhas: {stickers.length}</h1>
+              <div >
+                <button className="add outline contained rounded"><FaArrowRight /></button>
+              </div>
+            </div>
+          </article>
+
         </div>
       </main>
     </>
