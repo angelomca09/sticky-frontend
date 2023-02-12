21060411a0d6ffef296c890c642e4d5b7c277f82
Angelo Munhoz <angelomca09@gmail.com>
2023-02-12 20:34:34 -0300

    Adding another Interface for albums

--- src/interfaces/IAlbum.ts
+++ src/interfaces/IAlbum.ts
@@ -6,3 +6,10 @@ export interface IAlbum {
   pages: number,
   stickers: string[]
 }
+
+export interface IAlbumWithStickers {
+  _id: string,
+  name: string,
+  pages: number,
+  stickers: ISticker[]
+}
