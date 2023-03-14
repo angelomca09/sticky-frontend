3ca1994e784e581d2f760aa436125251d607394c
Angelo Munhoz <angelomca09@gmail.com>
2023-03-14 18:20:18 -0300

    Making outline style a button component option

--- src/layout/Button/index.tsx
+++ src/layout/Button/index.tsx
@@ -6,12 +6,13 @@ interface IButtonProps {
   onClick?: () => void;
   contrasted?: boolean;
   loading?: boolean;
+  outlined?: boolean;
 }
 
-export const Button: React.FC<React.PropsWithChildren & IButtonProps> = ({ children, onClick, contrasted = false, loading = false }) => {
+export const Button: React.FC<React.PropsWithChildren & IButtonProps> = ({ children, onClick, contrasted = false, loading = false, outlined = false }) => {
   return (
     <div className='custom-button'>
-      <button className={`add outline contained rounded ${contrasted ? "contrast" : ""}`} aria-busy={loading}
+      <button className={`add contained rounded ${contrasted ? "contrast" : ""} ${outlined ? "outlined" : ""}`} aria-busy={loading}
         title="Voltar" onClick={onClick}>
         {loading ? "" : (children ?? <FaStickyNote />)}
       </button>
